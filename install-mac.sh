15d0d07b3d4bb9e13d6c7f029d2290059466943a
Joe Corcoran <joe@corcoran.io>
2016-04-19 11:28:27 +0200

    Create install-mac.sh

---
+++ install-mac.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Essentials
+brew install git tmux vim ag
+
+# Setup
+if [ ! -d $HOME/Projects ]; then
+  mkdir $HOME/Projects
+fi
+if [ ! -d $HOME/Projects/setup ]; then
+  git clone https://github.com/joecorcoran/setup $HOME/Projects/setup
+fi
+
+# Vundle
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+files=".bashrc .gitconfig .tmux.conf .vimrc"
+for file in $files; do
+  ln -s $HOME/Projects/setup/$file $HOME/$file
+done
+
+# Install Vim plugins
+vim +PluginInstall +qall
