38efcb80a44fd31e1ee249b3fa001d61ffca8bbf
Joe Corcoran <joe@corcoran.io>
2019-08-19 15:19:19 +0200

    Update install-mac.sh

--- install-mac.sh
+++ install-mac.sh
@@ -4,7 +4,7 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Essentials
-brew install git tmux nvim ruby-install chruby ripgrep
+brew install git tmux vim ruby-install chruby ripgrep
 
 # Setup
 if [ ! -d $HOME/Projects ]; then
